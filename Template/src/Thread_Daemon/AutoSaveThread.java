1b709df248e0c11f2fd2f939ba3ad3b0c3a1aa07
Jang Sung-woo <67782250+seeminglyjs@users.noreply.github.com>
2020-11-20 23:22:08 +0900

    Add files via upload

---
+++ Template/src/Thread_Daemon/AutoSaveThread.java
@@ -0,0 +1,19 @@
+package Thread_Daemon;
+
+public class AutoSaveThread extends Thread{
+	public void save() {
+		System.out.println("작업내용을 저장");
+	}
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				break;
+			}
+			save();
+		}
+
+	}
+}
---
+++ Template/src/Thread_Daemon/main.java
@@ -0,0 +1,19 @@
+package Thread_Daemon;
+
+public class main {
+
+	public static void main(String[] args) {
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+		
+		}
+		
+		System.out.println("메인 스레드 종료");
+	}
+
+}
